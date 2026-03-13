9148d2c7ab04faa3f2ab16942aa762bb9d82e6f0
kautsar rahman <kautsar.rahman@gmail.com>
2026-03-13 17:28:13 +0900

    Navbar Component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
+  <Navbar />
   <RouterView />
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components: { Navbar }
+}
+
+</script>
 
 <style>
 body {
