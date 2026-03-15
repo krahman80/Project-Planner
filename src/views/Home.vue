f8b8725cdacb0b4aeb7009de507054038883cb46
kautsar rahman <kautsar.rahman@gmail.com>
2026-03-15 13:19:18 +0900

    uisng watch and watchEffect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,12 @@
   <div v-for="name in matchingNames" :key="name">
   {{ name }}
   </div>
-
+  <button @click="handleClick">stop watching</button>
 </template>
 
 <script>
+import { watchEffect } from 'vue';
+import { watch } from 'vue';
 import { computed, ref } from 'vue';
 
 export default {
@@ -17,11 +19,23 @@ export default {
     const search = ref('')
     const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
     
+    const stopWatch = watch(search, () => {
+      console.log('watch function run')
+    })
+
+    const stopEffect = watchEffect(() => {
+      console.log('watchEffect function run', search.value)
+    })
+
     const matchingNames = computed(() => {
       return names.value.filter((name) => name.includes(search.value))
     })
 
-    return { search, names, matchingNames }
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
+    return { search, names, matchingNames, handleClick }
   } 
 }
 </script>
