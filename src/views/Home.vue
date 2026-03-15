8c8bc728a46d264b63a3e780a56210eb66c67529
kautsar rahman <kautsar.rahman@gmail.com>
2026-03-15 09:26:23 +0900

    Using template refs in setup function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,30 @@
 <template>
 Home
-<p>my name is {{ name }} and age {{ age }}</p>
+<p ref="p">my name is {{ name }} and age {{ age }}</p>
 <button @click="handleClick">Click me</button>
 </template>
 
 <script>
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
   setup() {
-    console.log('setup')
+    console.log(this)
+
+    const p = ref('hello')
+    console.log(p, p.value)
 
     let name = 'mario'
     let age = 30
 
     const handleClick = () => {
-      console.log('you clicked me')
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'hello, ninjas'
     }
 
-    return { name, age, handleClick  }
+    return { name, age, handleClick, p  }
   } 
 }
 </script>
